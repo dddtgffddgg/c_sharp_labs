d1b367231f5c6410e6fdfafc150f349131003112
dddtgffddgg <115697740+dddtgffddgg@users.noreply.github.com>
2023-09-14 15:25:53 +0500

    lab1c.cs

--- lab1/lab1c.cs
+++ lab1/lab1c.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.IO.Pipes;
 using System.Runtime.InteropServices;
-using System.Runtime.CompilerServices;
 
 struct Message
 {
@@ -15,25 +12,50 @@ class PipeClient
 {
     static void Main(string[] args)
     {
-        Message message = new Message();
-        message.Data = 10;
-        message.Result = false;
-
-        using (var newClient = new NamedPipeClientStream("Pipe_lab1"))
+        using (var pipeClient = new NamedPipeClientStream(".", "Pipe_lab1", PipeDirection.InOut))
         {
-            byte[] bytes = new byte[Unsafe.SizeOf<Message>()];
-            Console.WriteLine("Client is working...");
-            newClient.Connect();
-            newClient.Read(bytes);
-            message = MemoryMarshal.Read<Message>(bytes);
-            message.Result = true;
-            MemoryMarshal.Write(bytes, ref message);
-            Console.WriteLine("{0}, {1}", message.Result, message.Data);
-            newClient.Write(bytes);
-            Console.WriteLine("{0}, {1}", message.Result, message.Data);
+            Console.WriteLine("Client is connecting...");
+            pipeClient.Connect();
+
+            Message sentMessage = new Message { Data = 10, Result = false };
+            WriteMessage(pipeClient, sentMessage);
+
+            Message receivedMessage = ReadMessage(pipeClient);
+            Console.WriteLine("Client received data: Result = {0}, Data = {1}", receivedMessage.Result, receivedMessage.Data);
         }
+
         Console.WriteLine("Client's work is done");
         Console.ReadLine();
     }
-}
 
+    static Message ReadMessage(NamedPipeClientStream pipeStream)
+    {
+        byte[] buffer = new byte[Marshal.SizeOf<Message>()];
+        pipeStream.Read(buffer, 0, buffer.Length);
+        return ByteArrayToMessage(buffer);
+    }
+
+    static void WriteMessage(NamedPipeClientStream pipeStream, Message message)
+    {
+        byte[] buffer = MessageToByteArray(message);
+        pipeStream.Write(buffer, 0, buffer.Length);
+        pipeStream.WaitForPipeDrain();
+    }
+
+    static Message ByteArrayToMessage(byte[] buffer)
+    {
+        GCHandle handle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+        Message message = (Message)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(Message));
+        handle.Free();
+        return message;
+    }
+
+    static byte[] MessageToByteArray(Message message)
+    {
+        byte[] buffer = new byte[Marshal.SizeOf(message)];
+        GCHandle handle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+        Marshal.StructureToPtr(message, handle.AddrOfPinnedObject(), false);
+        handle.Free();
+        return buffer;
+    }
+}
