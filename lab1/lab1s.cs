6aa842b6de9e883282ad336e122576a51453cacb
dddtgffddgg <115697740+dddtgffddgg@users.noreply.github.com>
2023-09-14 16:11:08 +0500

    lab1s.cs

--- lab1/lab1s.cs
+++ lab1/lab1s.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO.Pipes;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 struct Message
@@ -12,13 +13,13 @@ class PipeServer
 {
     static void Main(string[] args)
     {
-        using (var pipeServer = new NamedPipeServerStream("Pipe_lab1"))
+        using (var pipeServer = new NamedPipeServerStream("C_Sharp_Labs"))
         {
             Console.WriteLine("Server is waiting for a connection...");
             pipeServer.WaitForConnection();
 
             Message receivedMessage = ReadMessage(pipeServer);
-            Console.WriteLine("Server received data: Result = {0}, Data = {1}", receivedMessage.Result, receivedMessage.Data);
+            Console.WriteLine("Result = {0}, Data = {1}", receivedMessage.Result, receivedMessage.Data);
 
             receivedMessage.Result = true;
             WriteMessage(pipeServer, receivedMessage);
@@ -30,32 +31,18 @@ class PipeServer
 
     static Message ReadMessage(NamedPipeServerStream pipeStream)
     {
-        byte[] buffer = new byte[Marshal.SizeOf<Message>()];
+        byte[] buffer = new byte[Unsafe.SizeOf<Message>()];
         pipeStream.Read(buffer, 0, buffer.Length);
-        return ByteArrayToMessage(buffer);
+        return MemoryMarshal.Read<Message>(buffer);
     }
 
     static void WriteMessage(NamedPipeServerStream pipeStream, Message message)
     {
-        byte[] buffer = MessageToByteArray(message);
+        byte[] buffer = new byte[Unsafe.SizeOf<Message>()];
+
+        MemoryMarshal.Write(buffer, ref message);
+
         pipeStream.Write(buffer, 0, buffer.Length);
-        pipeStream.WaitForPipeDrain();
-    }
-
-    static Message ByteArrayToMessage(byte[] buffer)
-    {
-        GCHandle handle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
-        Message message = (Message)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(Message));
-        handle.Free();
-        return message;
-    }
-
-    static byte[] MessageToByteArray(Message message)
-    {
-        byte[] buffer = new byte[Marshal.SizeOf(message)];
-        GCHandle handle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
-        Marshal.StructureToPtr(message, handle.AddrOfPinnedObject(), false);
-        handle.Free();
-        return buffer;
+        pipeStream.Flush();
     }
 }
