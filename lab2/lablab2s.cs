0789f66250abf02a527caf1e371ba5cfe4f72c9a
dddtgffddgg <diana.bakieva69@gmail.com>
2023-10-28 12:02:35 +0500

    Update lablab2s.cs

--- lab2/lablab2s.cs
+++ lab2/lablab2s.cs
@@ -3,6 +3,7 @@ using System.IO.Pipes;
 using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks; // Добавляем пространство имен для работы с задачами
 
 struct Message
 {
@@ -10,36 +11,35 @@ struct Message
     public int valueB { get; set; }
     public int Priority { get; set; }
 }
-//добавить в очередь и после этого добавить две асинхронные задачи 
+
 class PipeServer
 {
     static List<Message> messageBuffer = new List<Message>();
 
-    async static Task Main(string[] args)
+    async static Task Main(string[] args) // Метод Main теперь асинхронный
     {
         using (var pipeServer = new NamedPipeServerStream("C_Sharp2"))
         {
             Console.WriteLine("Server is waiting for a connection...");
-            pipeServer.WaitForConnectionAsync();
+            await pipeServer.WaitForConnectionAsync(); // Используем асинхронный метод ожидания подключения
 
             Console.WriteLine("Введите данные и приоритет (Ctrl+C для завершения).");
 
-            
-            Console.CancelKeyPress += (sender, e) =>
+            Console.CancelKeyPress += async (sender, e) =>
             {
-                e.Cancel = true; 
-                pipeServer.Close(); 
+                e.Cancel = true;
+                pipeServer.Close();
 
-                Console.WriteLine("Saving valueB to file or displaying on screen:");
+                Console.WriteLine("Сохранение valueB в файл или отображение на экране:");
 
                 messageBuffer = messageBuffer.OrderByDescending(message => message.Priority).ToList();
 
                 foreach (var message in messageBuffer)
                 {
-
                     Console.WriteLine($"valueA = {message.valueA}, valueB = {message.valueB}, Priority = {message.Priority}");
                 }
 
+                await Task.Delay(1000); // Ожидаем 1 секунду перед завершением программы
                 Environment.Exit(0);
             };
 
@@ -47,8 +47,8 @@ class PipeServer
             {
                 try
                 {
-                    int valueB = int.Parse(Console.ReadLine());
                     int valueA = int.Parse(Console.ReadLine());
+                    int valueB = int.Parse(Console.ReadLine());
                     int priority = int.Parse(Console.ReadLine());
 
                     await AddMessageToQueueAsync(valueA, valueB, priority);
@@ -63,7 +63,7 @@ class PipeServer
 
     static async Task AddMessageToQueueAsync(int valueA, int valueB, int priority)
     {
-        Message recievedMessage = new Message {valueA = valueA, valueB = valueB, Priority = priority};
-        messageBuffer.Add(recievedMessage);
+        Message receivedMessage = new Message { valueA = valueA, valueB = valueB, Priority = priority };
+        messageBuffer.Add(receivedMessage);
     }
 }
