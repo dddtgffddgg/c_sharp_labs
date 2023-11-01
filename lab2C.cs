9f84a08eaedfab0ffc17e8cd0a67d0dd98949067
dddtgffddgg <diana.bakieva69@gmail.com>
2023-11-01 19:24:22 +0500

    Update lab2C.cs

--- lab2C.cs
+++ lab2C.cs
@@ -7,47 +7,75 @@ struct Message
 {
     public int valueA { get; set; }
     public int valueB { get; set; }
-    public int Priority { get; set; }
 }
 
 class PipeClient
 {
-    private static async Task Main(string[] args)
+    private static async Task Main()
     {
+        string filePath = "C:\\Users\\diana\\C_sharp_labs\\Lab2.txt";
+
         using (var pipeClient = new NamedPipeClientStream(".", "MyNamedPipe", PipeDirection.InOut))
         {
-            try
+            Console.WriteLine("The client is connecting...");
+            await pipeClient.ConnectAsync();
+
+            var dataBuffer = new StringWriter();
+
+            Console.CancelKeyPress += (sender, e) =>
             {
-                Console.WriteLine("Client is connecting...");
-                pipeClient.Connect();
+                e.Cancel = true;
+            };
 
-                Console.CancelKeyPress += (sender, e) => //обработчик для Ctrl+C
-                {
-                    e.Cancel = true; 
-
-                    pipeClient.Close();
-                };
-
-                while (true)
+            while (true)
+            {
+                try
                 {
                     Message receivedMessage = ReadMessage(pipeClient);
-                    Console.WriteLine("Received Message: Value A = {0}, Value B = {1}, Priority = {2}", receivedMessage.valueA, receivedMessage.valueB, receivedMessage.Priority);
+                    Console.WriteLine("Received Message: Value A = {0}, Value B = {1}", receivedMessage.valueA, receivedMessage.valueB);
 
                 }
+                catch (InvalidDataException ex)
+                {
+                    Console.WriteLine("Error reading the message: " + ex.Message);
+                }
+                
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An error occurred: {ex.Message}");
-            }
+
+            Console.WriteLine("The client's work is completed");
         }
-
-        Console.WriteLine("Client's work is done");
     }
-    private static Message ReadMessage(NamedPipeClientStream pipeStream)
+
+    static Message ReadMessage(NamedPipeClientStream pipeStream)
     {
-        byte[] buffer = new byte[Unsafe.SizeOf<Message>()];
-        pipeStream.Read(buffer, 0, buffer.Length);
-        return MemoryMarshal.Read<Message>(buffer);
-    }
+        while (true) 
+        {
+            try 
+            {
+                var message = new Message();//try - catch Exepcion
+                var buffer = new byte[Marshal.SizeOf(message)];
+                int bytesRead = pipeStream.Read(buffer, 0, buffer.Length);
 
+                if (bytesRead == buffer.Length)
+                
+                {
+                    message.valueA = BitConverter.ToInt32(buffer, 0);
+                    message.valueB = BitConverter.ToInt32(buffer, sizeof(int));
+                    return message;
+                }
+
+                else
+
+                {
+                    throw new InvalidOperationException("Failed to read the message.");
+                }
+
+            } 
+            catch(InvalidDataException ex)
+            {
+                Console.WriteLine("Error reading the message: " + ex.Message);
+            }
+            
+        }
+    }
 }
